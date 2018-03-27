cee64de1057726b13149198aacfb312a374836c4
Arman Sujoyan <asujoyan@gmail.com>
2018-03-27 19:57:56 +0400

    Added solution for the problem triangle.js

---
+++ Sorting/triangle.js
@@ -0,0 +1,10 @@
+const triangle = (A)=>{
+    sorted  = A.sort((a,b) => {return a-b});
+    
+    for(let i=0;i<A.length-2;i++){
+        if((A[i] + A[i+1] > A[i+2]) && (A[i+1] + A[i+2] > A[i]) && (A[i] + A[i+2] > A[i+1]))
+            return 1;
+    }
+    
+    return 0;
+}
